026d58c2c548cc61d674d4c880ef21bd6f5a72ab
David Brick <david@davidbrick.com>
2025-12-15 20:59:21 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by David Brick on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
